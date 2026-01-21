b8f95f076f094fa069678465eb7be317796bb914
Manzela <81286733+Manzela@users.noreply.github.com>
2026-01-21 22:52:39 +0200

    chore: Remove intentional failure tests to unblock release

--- tests/test_logic_error.py
+++
@@ -1,7 +0,0 @@
-def test_pricing_logic():
-    """Verifies discount calculation."""
-    price = 100
-    discount = 0.20
-    # INTENTIONAL BUG: Subtracting rate instead of value
-    final_price = price - discount 
-    assert final_price == 80
